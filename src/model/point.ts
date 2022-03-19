1a49d0acd7591352745b318eae4e1afadece3f1b
sanfordliao <sanfordliao@tencent.com>
2022-03-19 20:22:56 +0800

    feat: 选区相关接口的实现

---
+++ src/model/point.ts
@@ -0,0 +1,56 @@
+import { Path } from "./path";
+
+export const isPoint = (value: any): value is Point => value instanceof Point;
+
+export class Point {
+  constructor(public path: Path, public offset: number) {
+
+  }
+
+  /**
+   * Compare a point to another, returning an integer indicating whether the
+   * point was before(-1), at(0), or after(1) the other.
+   * @param another 
+   * @returns 
+   */
+  compare(another: Point) {
+    const result = Path.compare(this.path, another.path)
+
+    if (result === 0) {
+      if (this.offset < another.offset) return -1
+      if (this.offset > another.offset) return 1
+      return 0
+    }
+
+    return result
+  }
+
+  /**
+   * Check is after another.
+   * @param another 
+   * @returns 
+   */
+  isAfter(another: Point) {
+    return this.compare(another) === 1;
+  }
+
+  /**
+   * Check is before another.
+   * @param another 
+   * @returns 
+   */
+  isBefore(another: Point) {
+    return this.compare(another) === -1;
+  }
+
+  equals(another: Point): boolean {
+    // PERF: ensure the offsets are equal first since they are cheaper to check.
+    return (
+      this.offset === another.offset && Path.equals(this.path, another.path)
+    )
+  };
+
+  clone() {
+    return new Point(this.path.slice(), this.offset);
+  }
+}
---
+++ src/model/range.ts
@@ -0,0 +1,63 @@
+import { Path } from "./path";
+import { Point } from "./point";
+
+export class Range {
+  constructor(public start: Point, public end: Point) {
+  }
+
+  /**
+   * Check if a range is exactly equal to another.
+   */
+   equals(another: Range): boolean {
+    const { start, end } = this;
+    return (
+      start.equals(another.start)
+      && end.equals(another.end)
+    );
+  }
+
+  
+  /**
+   * Check if a range is collapsed, meaning that both its anchor and focus
+   * points refer to the exact same position in the document.
+   * @returns 
+   */
+  isCollapsed(): boolean {
+    const { start, end } = this
+    return start.equals(end);
+  };
+
+
+  includeRange(target: Range) {
+    return this.includePoint(target.start)
+  }
+
+  includePath(path: Path) {
+    const { start, end } = this;
+
+    return Path.compare(path, start.path) >= 0 && Path.compare(path, end.path) <= 0;
+
+  }
+
+  includePoint(point: Point) {
+    const { start, end } = this;
+    return point.compare(start) >= 0 && point.compare(end) <= 0;
+  }
+
+  /**
+   * Get the intersection of a range with another.
+   * 获取和另一个range的交集
+   */
+   intersection(another: Range): Range | null {
+    const { start: s1, end: e1 } = this
+    const { start: s2, end: e2 } = another;
+    const start = s1.isBefore(s2) ? s2 : s1
+    const end = e1.isBefore(e2) ? e1 : e2
+
+    if (end.isBefore(start)) {
+      return null
+    } else {
+      return new Range(start.clone(), end.clone())
+    }
+  }
+}
